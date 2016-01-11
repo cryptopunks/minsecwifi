4740fc3c3b2033e12791d919db4a0fd4cf7d7411
Sakałoŭ Alaksiej <nullbsd@gmail.com>
2016-01-11 22:08:25 +0300

    check_conf add and add install/uninstall script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# (C) 2015-2016 Cryptopunks.org <Xa2ee at cryptopunks dot org>
+#
+# MinSecWifi installer
+
+INSTALLDIR="/usr/bin"
+
+# check root
+if [[ "$USER" != 'root' ]]; then
+    echo "Sorry, you need to run this as root"
+    exit
+fi
+
+# Copying minsecwifi in $INSTALLDIR
+echo -n "Copying "
+cp -v minsecwifi $INSTALLDIR
+# Replacing path to minsecwifi.conf
+sed -i s/'minsecwifi.conf'/'\/etc\/minsecwifi.conf'/ $INSTALLDIR/minsecwifi
+# Generate new config
+if [ ! -f /etc/minsecwifi.conf ]; then
+    $INSTALLDIR/minsecwifi -xm > /etc/minsecwifi.conf
+fi
+
+echo
+echo "Please edit /etc/minsecwifi.conf (run: \`sudo $INSTALLDIR/minsecwifi -em\`)"
---
+++ uninstall.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# (C) 2015-2016 Cryptopunks.org <Xa2ee at cryptopunks dot org>
+#
+# MinSecWifi uninstall script
+
+INSTALLDIR="/usr/bin"
+
+# check root
+if [[ "$USER" != 'root' ]]; then
+    echo "Sorry, you need to run this as root"
+    exit
+fi
+
+# check minsecwifi
+if [ -f $INSTALLDIR/minsecwifi ]; then
+    echo "Removing $INSTALLDIR/minsecwifi: "
+    rm -v $INSTALLDIR/minsecwifi
+else
+    echo "$INSTALLDIR/minsecwifi not installed"
+fi
+
+# check config
+if [ -f /etc/minsecwifi.conf ]; then
+    echo
+    echo "Remove /etc/minsecwifi.conf? (y/n)"
+    read yesno
+    if [ $yesno = "y" ]; then
+        rm /etc/minsecwifi.conf
+    fi
+fi
+
